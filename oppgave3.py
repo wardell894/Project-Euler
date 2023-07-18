e7fab5ddd6ae30655b4dd11b01fe5536adcadf27
steinar jonathan Mpagi <67162446+wardell894@users.noreply.github.com>
2023-07-18 15:42:31 +0200

    Update oppgave3.py

--- oppgave3.py
+++ oppgave3.py
@@ -1,14 +1,22 @@
-#her var jeg såpass frekk og gikk bare for oddetall istedenfor heleveien for å finne primtallsfaktorer
 from functools import reduce
 def largest_prime_factor(N, F):
     faktor_liste = []
     for i in range(1,N):
         K = 2*i+1
-        if (F%K == 0):
+        if (F%K == 0 and erPrimtall(K) == True):
             print(K)
             faktor_liste.append(K)
     result1 = reduce((lambda x, y: x * y), faktor_liste)
     return result1
+
+def erPrimtall(N):
+    for i in range(2,N):
+        tester = True
+        if(N%i==0):
+            print(N, "kan deles paa", i)
+            tester = False
+            break
+    return tester
+     
+       
 print(largest_prime_factor(10000, 600851475143))
-#print(71*839*1471*6857)
-print(600851475143)
